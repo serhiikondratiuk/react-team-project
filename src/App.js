7bb59b4fba78ee63d66ca9d5bc38592e95fdd860
Bogdan-Kasyanchuk <bogdan.k1990@gmail.com>
2021-11-28 18:13:50 +0200

    add navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Logo from './components/Logo/Logo';
+import Navigation from './components/Navigation/Navigation';
 
 function App() {
   return (
     <>
       <Logo children="Product" />
+      <Navigation />
     </>
   );
 }
