830f308645b8fa36beb9f8fa616854c4c85cfe04
Fosa1990 <fosa1990@gmail.com>
2021-12-04 22:27:37 +0200

    Update Section.js

--- src/components/base/Section/Section.js
+++ src/components/base/Section/Section.js
@@ -1,32 +1,16 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { getStyles } from '../../../layout/layouts';
-const styles = getStyles();
 
 const SectionWrapper = styled.section`
   padding: 0 25px;
   margin: 0;
 `;
 
-const SectionTitle = styled.h2`
-  font-size: 32px;
-  font-weight: bold;
-  line-height: 1.5;
-  text-align: center;
-  color: ${styles.mainFontColor};
-`;
-
-const Section = ({ title, children }) => {
-  return (
-    <SectionWrapper>
-      <SectionTitle>{title}</SectionTitle>
-      {children}
-    </SectionWrapper>
-  );
+const Section = ({ className, children }) => {
+  return <SectionWrapper className={className}>{children}</SectionWrapper>;
 };
 
 Section.propTypes = {
-  title: PropTypes.string,
   children: PropTypes.node,
 };
 
