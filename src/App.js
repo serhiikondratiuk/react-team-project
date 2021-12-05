b2dd58569076d97a76b2b1ffd6e236ee948ae5f7
MariaShebeko <marygro89@gmail.com>
2021-12-05 15:48:49 +0200

    added Feature-section, added index.js for components for shorting pathes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import Container from './components/base/Container/Container';
-import Section from './components/base/Section/Section';
+import Container from './components/base/Container';
+import Section from './components/base/Section';
 import Logo from './components/Logo/Logo';
 import Navigation from './components/Navigation/Navigation';
 import Home from './pages/Home/Home';
 import FooterBottomPart from './components/Footer/Footer';
 import CustomerSupport from './pages/customerSupport/';
+import FeaturesList from './pages/Features';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Home />
         </Container>
       </Section>
+      <FeaturesList />
       <CustomerSupport />
       <Container>
         <FooterBottomPart />
